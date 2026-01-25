5cff836f62a28a21d40db0bcbe981c1d90fe360a
phwnganh <phuonganh25102003@gmail.com>
2026-01-25 18:03:58 +0700

    fix: optimize responsive testimonial section

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -4,11 +4,11 @@ import JeremyLargeImg from '../assets/image-jeremy-large.png'
 const TestimonialSection = () => {
     return (
         <section className={"mt-47.5 max-w-277.5 mx-auto sm:px-8 md:px-0"}>
-            <div className={"bg-neutral-900 rounded-15 px-4 sm:py-16 sm:px-12 md:px-31.25 py-22 relative z-10 flex flex-col items-center md:block"}>
-                <img src={JeremyLargeImg} alt={"jeremy-large"} className={"mx-auto -mt-32 md:absolute md:bottom-0 md:mt-0"}/>
-                <img src={PatternCurvedLine2} alt={"pattern-curved-line2"} className={"my-9 md:my-0 md:absolute md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 z-10"}/>
+            <div className={"bg-neutral-900 rounded-15 px-4 sm:py-16 sm:px-12 md:px-31.25 py-22 relative z-10 flex flex-col items-center lg:block"}>
+                <img src={JeremyLargeImg} alt={"jeremy-large"} className={"mx-auto -mt-32 lg:absolute lg:bottom-0 lg:mt-0"}/>
+                <img src={PatternCurvedLine2} alt={"pattern-curved-line2"} className={"my-9 lg:my-0 lg:absolute lg:left-1/2 lg:top-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2 z-10"}/>
 
-                <div className={"flex items-center justify-end relative z-20 md:ml-115 text-center md:text-left"}>
+                <div className={"flex items-center justify-end relative z-20 lg:ml-115 text-center lg:text-left"}>
                     <div className={"flex flex-col gap-6 max-w-87.5 w-full"}>
                         <div className={"flex flex-col gap-8"}>
                             <p className={"text-neutral-0 text-preset-2 leading-preset-2 tracking-preset-2"}>It just <span className={"font-bold"}>works.</span></p>
